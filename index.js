c317f2bcfc9005bc4b9d00a13f547bda22e66b2a
Maja Reichert <stu126115@mail.uni-kiel.de>
2020-12-28 11:44:29 +0100

    Turn nav bar solid on scroll

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+// Clicking on the mobile menu button will open the navigation on the same page.
 function openMenu() {
   let menuContainer = document.querySelector('.navigation');
   menuContainer.classList.add('mobile-menu-window');
 }
 
-var menuButton = document.querySelector('.mobile-menu-button');
-menuButton.onclick = openMenu;
+function handleScrolling() {
+  let nav = document.getElementById('navigation');
+  if (window.pageYOffset) {
+    nav.classList.remove('navigation--transparent');
+    nav.classList.add('navigation--solid');
+  } else {
+    nav.classList.add('navigation--transparent');
+    nav.classList.remove('navigation--solid');
+  }
+}
+
+function main() {
+  var menuButton = document.querySelector('.mobile-menu-button');
+  menuButton.onclick = openMenu;
+  // When the website is scrolled down at all, the navigation turns solid.
+  // At least I think that's what the design says it should do.
+  window.onscroll = handleScrolling;
+}
+
+window.onload = main;
