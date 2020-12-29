f4145c18321f05c091147b43e363995e2a3e8933
Maja Reichert <stu126115@mail.uni-kiel.de>
2020-12-29 16:58:13 +0100

    Trap focus when the mobile menu overlay is open

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var navigation, focusableItems, firstItem, lastItem;
+
 // Clicking on the mobile menu button will open the navigation on the same page.
 function openMenu() {
   var menuContainer = document.getElementById('navigation');
@@ -9,6 +11,7 @@ function openMenu() {
   menuButton.classList.add('u-hidden');
   // Disable vertical scrolling
   document.body.classList.add('u-disable-scroll');
+  trapFocusInMenu();
 }
 
 function closeMenu() {
@@ -21,8 +24,11 @@ function closeMenu() {
   menuButton.classList.remove('u-hidden');
   // Enable vertical scrolling
   document.body.classList.remove('u-disable-scroll');
+  releaseMenuFocusTrap();
 }
 
+// When the website is scrolled down at all, the navigation turns solid.
+// At least I think that's what the design says it should do.
 function handleScrolling() {
   let nav = document.getElementById('navigation');
   if (window.pageYOffset) {
@@ -34,6 +40,31 @@ function handleScrolling() {
   }
 }
 
+// Could be generalized with parameters, but we only need this keyboard trap for now.
+function trapFocusInMenu() {
+  firstItem.focus();
+  navigation.addEventListener('keydown', trapFocus);
+}
+
+function releaseMenuFocusTrap() {
+  navigation.removeEventListener('keydown', trapFocus);
+}
+
+// Listens for Tab or Shift + Tab and wraps focus around if necessary.
+function trapFocus(e) {
+  if (e.key == 'Tab') {
+    if (e.shiftKey && document.activeElement === firstItem) {
+      // wrap around backwards
+      e.preventDefault();
+      lastItem.focus();
+    } else if (!e.shiftKey && document.activeElement === lastItem) {
+      // wrap around forwards
+      e.preventDefault();
+      firstItem.focus();
+    }
+  }
+}
+
 function main() {
   var menuButton = document.getElementById('mobile-menu-button');
   var closeButton = document.getElementById('mobile-menu-close-button');
@@ -41,8 +72,6 @@ function main() {
   // Set up event listeners
   menuButton.onclick = openMenu;
   closeButton.onclick = closeMenu;
-  // When the website is scrolled down at all, the navigation turns solid.
-  // At least I think that's what the design says it should do.
   window.onscroll = handleScrolling;
 
   // Make sure that the mobile menu overlay is closed when a link is clicked in it.
@@ -50,6 +79,18 @@ function main() {
   menuLinks.forEach(function (el) {
     el.onclick = closeMenu;
   });
+
+  // Initialize global variables
+  navigation = document.getElementById('navigation');
+
+  // All focusable items in the mobile menu overlay as an array.
+  focusableItems = Array.prototype.slice.call(
+    document.querySelectorAll('.menu-link, #mobile-menu-close-button'),
+    0
+  );
+
+  firstItem = focusableItems[0];
+  lastItem = focusableItems[focusableItems.length - 1];
 
   // In case the page is already scrolled when it is (re-)loaded
   handleScrolling();
