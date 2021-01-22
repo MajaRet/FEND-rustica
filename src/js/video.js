8fccbdd950d2b944deed5dc88fad8d858a4c9798
Maja Reichert <stu126115@mail.uni-kiel.de>
2021-01-22 22:56:25 +0100

    Add basic play/pause functionality to play button #7

---
+++ src/js/video.js
@@ -0,0 +1,35 @@
+let playing = false;
+const playButtons = document.querySelectorAll(".play-button");
+
+function playVideo(video, playButton) {
+  playing = true;
+  video.play();
+  playButton.classList.add("playing");
+  playButton.classList.remove("paused");
+}
+
+function pauseVideo(video, playButton) {
+  playing = false;
+  video.pause();
+  playButton.classList.add("paused");
+  playButton.classList.remove("playing");
+}
+
+function playOrPauseVideo(video, playButton) {
+  if (playing) {
+    pauseVideo(video, playButton);
+  } else {
+    playVideo(video, playButton);
+  }
+  // TODO Change play button to pause button
+}
+
+playButtons.forEach(function assignPlayListener(btn) {
+  const video = document.getElementById(
+    `video-${btn.id.slice("play-button-".length)}`
+  );
+  // When the play button is clicked, toggle play/pause.
+  btn.addEventListener("click", playOrPauseVideo.bind(null, video, btn));
+  // Also perform the pausing actions when the video ends.
+  video.addEventListener("ended", pauseVideo.bind(null, video, btn));
+});
