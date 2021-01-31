e42993f8c513dd8969a19020277ff01262f78f08
Maja Reichert <stu126115@mail.uni-kiel.de>
2021-01-31 22:26:56 +0100

    Add comment to form js code

--- src/js/form.js
+++ src/js/form.js
@@ -1,6 +1,8 @@
 document.querySelectorAll("form").forEach((form) => {
   const submitButton = form.querySelector("input[type=submit]");
   if (submitButton) {
+    // On clicking the submit button, invalid form items should receive custom
+    // styles, so they are given the "required" class.
     submitButton.addEventListener("click", () =>
       form.querySelectorAll("input[required]").forEach((field) => {
         field.classList.add("required");
